1d9b91eeab1d77d2bdabacb6f09fd8aec7b80384
Michael Tamirie <michaeltamirie@gmail.com>
2022-04-06 10:18:01 +0300

    Finish book.js

---
+++ src/redux/books/books.js
@@ -0,0 +1,34 @@
+const ADD_BOOK = 'ADD_BOOK'
+const REMOVE_BOOK = 'REMOVE_BOOK'
+const initialState = []
+
+export const addBook = (book) => {
+  return {
+    type: ADD_BOOK,
+    payload: book
+  }
+}
+
+export const removeBook = (id) => {
+  return {
+    type: REMOVE_BOOK,
+    payload: id
+  }
+}
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK: return [
+      ...state,
+      {
+        id: 1,
+        title: action.payload.title,
+        author: action.payload.author
+      }
+    ]
+    case REMOVE_BOOK: return state.filter((book) => (book.id !== action.payload.id))
+    default: return state
+  }
+}
+
+export default bookReducer
