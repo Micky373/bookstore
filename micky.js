d066bd6e86b1f0a7b91cd88930297b2e8dd58c8c
Michael Tamirie <michaeltamirie@gmail.com>
2022-04-06 22:32:40 +0300

    Finish the add and remove functionality

---
+++ micky.js
@@ -0,0 +1,3 @@
+const array = [{ micky: 1, meddy: 2 }, { micky: 3, meddy: 4 }, { micky: 5, meddy: 6 }]
+const micky = array.splice(1, 1)
+console.log(array)
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,22 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'Book 1',
+    author: 'Author 1',
+  },
+  {
+    id: 2,
+    title: 'Book 2',
+    author: 'Author 2',
+  },
+  {
+    id: 3,
+    title: 'Book 3',
+    author: 'Author 3',
+  },
+];
 
 export const addBook = (book) => ({
   type: ADD_BOOK,
@@ -22,7 +38,14 @@ const bookReducer = (state = initialState, action) => {
         author: action.payload.author,
       },
     ];
-    case REMOVE_BOOK: return state.filter((book) => (book.id !== action.payload.id));
+    case REMOVE_BOOK: {
+      const newState = [...state];
+      newState.splice(action.payload - 1, 1);
+      for (let i = 0; i < newState.length; i += 1) {
+        newState[i].id = i + 1;
+      }
+      return newState;
+    }
     default: return state;
   }
 };
