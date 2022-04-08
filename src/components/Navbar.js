343efd2fe1829c07a70789c90c10086ac9a90cc2
Michael Tamirie <michaeltamirie@gmail.com>
2022-04-08 08:43:26 +0300

    Finish styling the nav-bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HeaderStyle = {
-  color: '#b6ad90',
+  width: '15rem',
+  height: '2.313rem',
+  margin: '0.375rem 2.938rem 0.125rem 0',
+  fontFamily: 'Montserrat',
+  fontSize: '1.875rem',
+  fontWeight: 'bold',
+  fontStretch: 'normal',
+  fontStyle: 'normal',
+  lineHeight: 'normal',
+  letterSpacing: 'normal',
+  color: '#0290ff',
 };
 
 const Navbar = () => (
   <nav>
-    <h1 style={HeaderStyle}>Bookstore</h1>
+    <h1 style={HeaderStyle}>Bookstore CMS</h1>
     <ul className="nav_links">
       <Link to="/"><li>Books</li></Link>
       <Link to="/Catagories"><li>Catagories</li></Link>
